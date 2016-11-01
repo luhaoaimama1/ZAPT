2b31bc4823985d9a183613d28f6ad7ddb73edc0a
fuzhipeng <1149324777@qq.com>
2016-11-01 17:46:39 +0800

    更改一些测试信息

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 #知识总结
+###初始化里的东西
+```
+messager:用来打印信息；注意：如果Diagnostic.Kind.ERROR的话build会失败；
+
+filer:
+
+elementUtils:获取包名的；等信息；
+
+typeUtils：  Element inter = typeUtils.asElement(typeMirror);
+可以把TypeMirror类型弄成 element 一般都是接口等信息（element.asType()）；后面有解释；
+
+```
+
+###获取element注解比较简单
+
+```
+element..getAnnotation(ZClass.class);
+```
 
 ###Package
 
@@ -7,8 +25,9 @@
 elementUtils.getPackageOf(element).getQualifiedName().toString();
 ```
 
-### Class
-
+### Class(TypeElement)
+if (element.getKind() == ElementKind.CLASS) 
+ 
 获取此的类型：
 ```
 element.asType()
@@ -66,8 +85,10 @@ child:onClick(android.view.View)	 kind->METHOD
 ```
 
 
-需要知道的事情:
+###需要知道的事情:
+
 转：http://www.race604.com/annotation-processing/
+
 这里有一点小麻烦，因为这里的类型是一个java.lang.Class。
 这就意味着，他是一个真正的Class对象。因为注解处理是在编译Java源代码之前。我们需要考虑如下两种情况： 
 
@@ -96,7 +117,8 @@ try {
 它表示我们未编译类。因为我们已经知道它必定是一个类类型（我们已经在前面检查过），
 我们可以直接强制转换为DeclaredType，然后读取TypeElement来获取合法的名字。
 
-### Field
+### Field（VariableElement）
+if (element.getKind() == ElementKind.FIELD)
 
 获取所在的类：
 ```
--- apt/src/main/java/com/example/ViewInjectProcessor.java
+++ apt/src/main/java/com/example/ViewInjectProcessor.java
@@ -59,6 +59,19 @@ public class ViewInjectProcessor extends AbstractProcessor {
 
                 sb.append("------------ start:"+support.getSimpleName()+"------------------------\n");
                 sb.append("packageName:" + LogUtils.getPackageName(elementUtils,  element) + "\n");
+
+                try {
+                    sb.append("kind类型:" + element.getKind() + "\n");
+                } catch (Exception e) {
+                    messager.printMessage(Diagnostic.Kind.ERROR, "kind类型:" + e.getMessage() + "\n");
+                }
+
+                try {
+                    sb.append("element.asType():" + element.asType().toString() + "\n");
+                } catch (Exception e) {
+                    messager.printMessage(Diagnostic.Kind.ERROR, "element.asType():" + e.getMessage() + "\n");
+                }
+
                 try {
                     sb.append("element:" + element.toString() + "\n");
                     sb.append("element_getModifiers:" + element.getModifiers().toString() + "\n");
@@ -100,21 +113,8 @@ public class ViewInjectProcessor extends AbstractProcessor {
                     }
                 } catch (Exception e) {
                     messager.printMessage(Diagnostic.Kind.ERROR, "element:" + e.getMessage() + "\n");
-                    messager.printMessage(Diagnostic.Kind.ERROR, "cause:" + e.getCause()+ "\n");
                 }
-//                try {
-//                    //类名（接口名）
-//                    TypeElement enclosingElement = (TypeElement) element.getEnclosingElement();
-//                    sb.append("类名（接口名)enclosingElement:" + enclosingElement.toString() + "\n");
-//                } catch (Exception e) {
-//                    messager.printMessage(Diagnostic.Kind.ERROR, "类名（接口名)enclosingElement:" + e.getMessage() + "\n");
-//                }
-                try {
-                    //类名（接口名）
-                    sb.append("kind类型:" + element.getKind() + "\n");
-                } catch (Exception e) {
-                    messager.printMessage(Diagnostic.Kind.ERROR, "kind类型:" + e.getMessage() + "\n");
-                }
+
                 try {
                     sb.append("EnclosedElements:" + element.getEnclosedElements().toString() + "\n");
                     for (Element element1 : element.getEnclosedElements())
@@ -122,11 +122,7 @@ public class ViewInjectProcessor extends AbstractProcessor {
                 } catch (Exception e) {
                     messager.printMessage(Diagnostic.Kind.ERROR, "EnclosedElements:" + e.getMessage() + "\n");
                 }
-                try {
-                    sb.append("element.asType():" + element.asType().toString() + "\n");
-                } catch (Exception e) {
-                    messager.printMessage(Diagnostic.Kind.ERROR, "element.asType():" + e.getMessage() + "\n");
-                }
+
                 sb.append("-------------end-----------------------\n");
                 sb.append("\n");
                 sb.append("\n");
@@ -134,51 +130,9 @@ public class ViewInjectProcessor extends AbstractProcessor {
         }
         LogUtils.writeLog2(sb.toString(), count);
         count++;
-//        //一个类 声明多次
-//        for (Element element : env.getElementsAnnotatedWith(ZMethod.class)) {
-//
-//        }
         return true;
     }
 
-//
-//        int size = env.getElementsAnnotatedWith(GetMessage.class).size();
-//        messager.printMessage(Diagnostic.Kind.NOTE,
-//                "Annotation class size = " + size);
-//        for (Element element : env.getElementsAnnotatedWith(GetMessage.class)) {
-//            PackageElement packageElement = (PackageElement) element
-//                    .getEnclosingElement();
-//            //获取该注解所在类的包名
-//            String packageName = packageElement.getQualifiedName().toString();
-//            TypeElement classElement = (TypeElement) element;
-//            //获取该注解所在类的类名
-//            String className = classElement.getSimpleName().toString();
-//            //获取该注解所在类的全类名
-//            String fullClassName = classElement.getQualifiedName().toString();
-//            //获取该注解的值
-//            String id = classElement.getAnnotation(GetMessage.class).id();
-//            String name = classElement.getAnnotation(GetMessage.class).name();
-////            messager.printMessage(Diagnostic.Kind.NOTE,
-////                    "Annotation class : packageName = " + "风了吧");
-//            messager.printMessage(Diagnostic.Kind.NOTE,
-//                    "Annotation class : packageName = " + packageName);
-//            messager.printMessage(Diagnostic.Kind.NOTE,
-//                    "Annotation class : className = " + className);
-//            messager.printMessage(Diagnostic.Kind.NOTE,
-//                    "Annotation class : fullClassName = " + fullClassName);
-//            messager.printMessage(Diagnostic.Kind.NOTE,
-//                    "Annotation class : id = " + id + "  name = " + name);
-//        }
-
-
-    //    1.Messager 用来输出。就像我们平时用的System.out.pringln()和Log.d。输出位置在编译器下方的Messages窗口。这里System.out也是可以用的哦~
-//
-//    2.用for循环遍历所有的 GetMsg注解，然后进行处理。
-//
-//    3.Diagnostic.Kind.NOTE 类似于Log.d LogUtils.e这样的等级。
-//
-//   4.return true;表示该Process已经处理了，其他的Process不需要再处理了。
-//
     @Override
     public Set<String> getSupportedAnnotationTypes() {
         Set<String> types = new LinkedHashSet<>();
